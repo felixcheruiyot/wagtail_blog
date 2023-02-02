570b92f1628ab393096295c757996b8bfd1ad007
FELIX CHERUIYOT <felix@intasend.com>
2023-02-02 10:55:42 +0300

    Enable wagtail cache

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,20 @@
 from django.urls import re_path
+from wagtailcache.cache import cache_page
+
 from . import views
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    re_path(r'^tag/(?P<tag>[-\w]+)/', views.tag_view, name="tag"),
-    re_path(r'^category/(?P<category>[-\w]+)/feed/$', views.LatestCategoryFeed(), name="category_feed"),
-    re_path(r'^category/(?P<category>[-\w]+)/', views.category_view, name="category"),
-    re_path(r'^author/(?P<author>[-\w]+)/', views.author_view, name="author"),
+    re_path(r'^tag/(?P<tag>[-\w]+)/', cache_page(views.tag_view), name="tag"),
+    re_path(r'^category/(?P<category>[-\w]+)/feed/$', cache_page(views.LatestCategoryFeed()), name="category_feed"),
+    re_path(r'^category/(?P<category>[-\w]+)/', cache_page(views.category_view), name="category"),
+    re_path(r'^author/(?P<author>[-\w]+)/', cache_page(views.author_view), name="author"),
     re_path(r'(?P<blog_slug>[\w-]+)/rss.*/',
-        views.LatestEntriesFeed(),
+        cache_page(views.LatestEntriesFeed()),
         name="latest_entries_feed"),
     re_path(r'(?P<blog_slug>[\w-]+)/atom.*/',
-        views.LatestEntriesFeedAtom(),
+        cache_page(views.LatestEntriesFeedAtom()),
         name="latest_entries_feed_atom"),
 ]
